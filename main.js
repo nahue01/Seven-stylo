2c234c23887907a5d19eea1377d1d2f7c34dc3d3
nahue01 <105022044+nahue01@users.noreply.github.com>
2026-03-29 07:55:02 -0300

    Add scroll-fade animation using IntersectionObserver

---
+++ main.js
@@ -0,0 +1,6 @@
+/* -------------------------------------------------
+   Simple scroll‑fade animation (IntersectionObserver)
+   ------------------------------------------------- */
+const fadeElems = document.querySelectorAll('.service-card, .gallery__item, .testimonial');
+
+co
